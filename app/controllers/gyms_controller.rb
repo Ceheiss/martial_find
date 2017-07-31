f52a24532354cd2903e5ecf3bfd579780b2e5d64
Cristobal Heiss <barbacoabq@gmail.com>
2017-07-31 13:44:01 -0400

    Add new and creat actions

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,5 +2,19 @@ class GymsController < ApplicationController
 
 	def index
 	end
-	
+
+	def new
+		@gym = Gym.new
+	end
+
+	def create
+		@gym = Gym.new(gym_params)
+	end
+
+	private
+
+	def gym_params
+		params.require(:gym).permit(:name, :description, :address)
+	end
+
 end
