8ebf32534ed4745349dc21d9f80ce22724e8e856
Cristobal Heiss <barbacoabq@gmail.com>
2017-08-01 22:18:18 -0400

    Created routes for reviews and review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-	resources :gyms
-
-	get '/categories/:id' => 'gyms#category', as: "category"
-
+    devise_for :users
+	resources :gyms do
+		resources :reviews
+	end
 	root 'gyms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+#get '/categories/:id' => 'gyms#category', as: "category". Not necessary anymore
